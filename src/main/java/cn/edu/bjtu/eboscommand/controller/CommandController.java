89a924a37becd3b48ca4559da712eed52d9b501b
Voo-C <voodoo.7@outlook.com>
2020-06-14 13:04:00 +0800

    refactor entity & polish swagger API doc

--- src/main/java/cn/edu/bjtu/eboscommand/controller/CommandController.java
+++ src/main/java/cn/edu/bjtu/eboscommand/controller/CommandController.java
@@ -1,6 +1,7 @@
 package cn.edu.bjtu.eboscommand.controller;
 
 import cn.edu.bjtu.eboscommand.entity.Command;
+import cn.edu.bjtu.eboscommand.entity.ListedCommand;
 import cn.edu.bjtu.eboscommand.service.LogService;
 import cn.edu.bjtu.eboscommand.service.MqFactory;
 import com.alibaba.fastjson.JSONArray;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 @Api(tags = "指令管理")
@@ -34,9 +36,9 @@ public class CommandController {
     @ApiOperation(value = "查看所属网关设备所支持的指令",notes = "用于创建指令期间要选择设备及相关资源时，填充下拉菜单")
     @CrossOrigin
     @GetMapping("/list")
-    public JSONArray checkCommand(){
+    public List<ListedCommand> checkCommand(){
         String allUrl = "http://"+ip+":48082/api/v1/device";
-        JSONArray commands = new JSONArray();
+        List<ListedCommand> listedCommands = new LinkedList<>();
         JSONArray all = new JSONArray(restTemplate.getForObject(allUrl,JSONArray.class));
         for(int i = 0; i<all.size();i++) {
             JSONObject deviceObj = all.getJSONObject(i);
@@ -46,17 +48,18 @@ public class CommandController {
             for(int j = 0; j < commandsArr.size();j++) {
                 String commandId = commandsArr.getJSONObject(j).getString("id");
                 String commandName = commandsArr.getJSONObject(j).getString("name");
-                JSONObject thisCommand = new JSONObject();
-                thisCommand.put("deviceId",deviceId);
-                thisCommand.put("deviceName",deviceName);
-                thisCommand.put("commandId",commandId);
-                thisCommand.put("commandName",commandName);
-                commands.add(thisCommand);
+                ListedCommand listedCommand = new ListedCommand();
+                listedCommand.setCommandId(commandId);
+                listedCommand.setCommandName(commandName);
+                listedCommand.setDeviceId(deviceId);
+                listedCommand.setDeviceName(deviceName);
+                listedCommands.add(listedCommand);
             }
         }
-        return commands;
+        return listedCommands;
     }
 
+    @ApiOperation(value = "向所属网关添加指令",notes = "所属网关为部署了本微服务的网关")
     @CrossOrigin
     @PostMapping()
     public String add(@RequestBody Command info){
@@ -70,6 +73,7 @@ public class CommandController {
         }
     }
 
+    @ApiOperation(value = "向所属网关恢复指令",notes = "与添加不同的是，其一可以批量恢复，其二添加是不允许重名，恢复时如果重名会选择覆盖策略")
     @CrossOrigin
     @PostMapping("/recover")
     public String plus(@RequestBody Command[] commands){
@@ -82,20 +86,23 @@ public class CommandController {
             return e.toString();}
     }
 
+    @ApiOperation(value = "向所属网关删除指令")
     @CrossOrigin
     @DeleteMapping()
     public boolean delete(@RequestParam String name){
         boolean flag = commandService.deleteCommand(name);
-        logService.info("删除了"+name+"指令");
+        logService.info("尝试删除"+name+"指令："+flag);
         return flag;
     }
 
+    @ApiOperation(value = "查看所属网关设备所有指令的详细信息", notes = "用于显示已有指令列表")
     @CrossOrigin
     @GetMapping()
     public List<Command> show(){
         return commandService.showAll();
     }
 
+    @ApiOperation(value = "微服务运行检测", notes = "微服务正常运行时返回 pong")
     @CrossOrigin
     @GetMapping("/ping")
     public String ping(){
